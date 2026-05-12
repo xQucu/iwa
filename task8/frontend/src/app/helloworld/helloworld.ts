4c32aa3ae0489c50a867c1f4071393eb6616865e
xqucu <kucykadrian2004@gmail.com>
2026-05-12 12:52:34 +0200

    feat(task8): add event binding

--- task8/frontend/src/app/helloworld/helloworld.ts
+++ task8/frontend/src/app/helloworld/helloworld.ts
@@ -1,17 +1,24 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {TokenStorageService} from '../auth/token-storage-service';
 import {NgTemplateOutlet} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-helloworld',
   imports: [
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    FormsModule
   ],
   templateUrl: './helloworld.html',
   styleUrl: './helloworld.css',
 })
 export class Helloworld {
   info: any;
+
+  onenterText = signal('');
+  oninputText = signal('');
+  onhoverText = signal('');
+  mousePosition = signal({x: 0, y: 0});
 
   private tokenStorage = inject(TokenStorageService);
 
@@ -21,6 +28,22 @@ export class Helloworld {
       username: this.tokenStorage.getUsername(),
       authorities: this.tokenStorage.getAuthorities()
     };
+  }
+
+  onEnter() {
+    this.onenterText.set('Mouse entered!');
+  }
+
+  onInput(value: string) {
+    this.oninputText.set(value);
+  }
+
+  onHover() {
+    this.onhoverText.set('Mouse hovered!');
+  }
+
+  onMouseMove(event: MouseEvent) {
+    this.mousePosition.set({x: event.clientX, y: event.clientY});
   }
 
   logout() {
